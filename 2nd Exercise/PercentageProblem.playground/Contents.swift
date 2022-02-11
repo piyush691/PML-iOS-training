b78af2d143f15b63b674219cde0286d461dce51f
piyush691 <piyushagrawal691@gmail.com>
2022-02-11 15:42:15 +0530

    Update Logic

--- 2nd Exercise/PercentageProblem.playground/Contents.swift
+++ 2nd Exercise/PercentageProblem.playground/Contents.swift
@@ -12,11 +12,11 @@ func solve(_ amount: [Double]) -> [Int]
     for i in amount
     {
         let eachStockPercentageWithoutRound = ((i*100)/total)
-        let eachStockPercentage = ((i*100)/total).rounded()
+        
+        let eachStockPercentage = min(Double(100-amount.count+1),max(1,((i*100)/total).rounded()))
         diff.append(eachStockPercentage - eachStockPercentageWithoutRound)
         percent.append(Int(eachStockPercentage))
     }
-    //    print(percent)
     // Add calculated RoundOff percentage of each stock
     let stockTotalPer = percent.reduce(0, +)
     
@@ -65,7 +65,10 @@ func solve(_ amount: [Double]) -> [Int]
     }
     
 }
-let amount: [Double] = [50, 70, 120, 110]
+let amounts: [[Double]] = [[50, 70, 120, 110],[99.2,0.4,0.4],[99.6,0.2,0.2],[99.6,0.1,0.1,0.1,0.1],[33,33,33],[61.25,38.75],[79.8,10.1,10.1],[79.2,10.4,10.4]]
 
-let percentage: [Int] = solve(amount)
-print("Each Stock percentage assigned: \(percentage)")
+for amount in amounts {
+    let percentage: [Int] = solve(amount)
+    print("Each Stock percentage assigned: \(percentage)")
+}
+
