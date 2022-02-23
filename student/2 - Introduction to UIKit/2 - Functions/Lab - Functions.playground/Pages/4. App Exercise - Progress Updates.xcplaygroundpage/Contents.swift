0b1fae13169ec6b4e40202e89ffe01a3e9fef9a4
piyush691 <piyushagrawal691@gmail.com>
2022-02-23 15:56:51 +0530

    Add Swift practice exercise

---
+++ student/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+
+//:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
+
+
+/*:
+[Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
+ */
