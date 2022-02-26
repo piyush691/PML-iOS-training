a6c910bacbb77953eaf18b39696cf88f1d3e9913
piyush691 <piyushagrawal691@gmail.com>
2022-02-26 16:58:49 +0530

    Complete loop exercise

--- student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
+++ student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -4,14 +4,22 @@
  Create a for-in loop that loops through values 1 to 100, and prints each of the values.
  */
 
+import Foundation
+for value in 1...100 {
+    print(value)
+}
 
 //:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
-
+for element in alphabet.enumerated() {
+    print("Index at \(element.offset) is \(element.element)")
+}
 
 //:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
-
-
+var India = ["MadhyaPradesh": "Bhopal", "Gujarat": "GandhiNagar", "Mizoram": "Imphal"]
+for value in India {
+    print(value.key, value.value)
+}
 /*:
 page 1 of 6  |  [Next: App Exercise - Movements](@next)
  */
